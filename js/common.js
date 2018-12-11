d5de31d1f820eb82c95b9c9fba72426dc1bb1592
Breeze8 <stashenkodenis@gmail.com>
2018-12-11 21:03:03 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,15 +1,58 @@
 $(function() {
 
+	$(".search input").focus(function(){
+        $('.search-mobile').css("visibility", "hidden");
+    });
+    $(".search input").blur(function(){
+        $('.search-mobile').css("visibility", "visible");
+    });
+
+	$( ".search-mobile" ).click(function() {
+	  $('.search').toggleClass('open');
+	});
+
+
+    $('.review-block .review-item .btn--main').click(function(j) {
+        var dropDown = $(this).closest('.review-item').find('.review-hidden');
+
+        $(this).closest('.review-block').find('.review-hidden').not(dropDown).slideUp();
+
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+        } else {
+            $(this).closest('.review-block').find('a.active').removeClass('active');
+            $(this).addClass('active');
+        }
+
+        dropDown.stop(false, true).slideToggle();
+
+        j.preventDefault();
+    });
+
+	$('.selectize').selectize({
+	    placeholder: 'Август',
+	  });
+
+	$('.selectize2').selectize({
+	    placeholder: '2018',
+	  });
+
+	$('.selectize3').selectize({
+	    placeholder: 'US  9',
+	  });
+
 	$('.toggler').click(function(){
 		$('.dropdown').toggleClass('active')
 	})
 
 	$('.header-menu svg').click(function(){
-		$('.menu').addClass('open')
+		$('.menu').addClass('open');
+		$('body').addClass('body-hidden')
 	})
 
 	$('.menu-close').click(function(){
-		$('.menu').removeClass('open')
+		$('.menu').removeClass('open');
+		$('body').removeClass('body-hidden')
 	})
 
 	$('.drop-slider').slick({
@@ -82,5 +125,25 @@ $(function() {
  	if($('.s-filter-nav button').hasClass("mixitup-control-active")) {
  		$('.s-filter-container').addClass("active");
  	}
+
+ 	document.querySelector("html").classList.add('js');
+
+	var fileInput  = document.querySelector( ".input-file" ),  
+	    button     = document.querySelector( ".input-file-trigger" ),
+	    the_return = document.querySelector(".file-return");
+	      
+	button.addEventListener( "keydown", function( event ) {  
+	    if ( event.keyCode == 13 || event.keyCode == 32 ) {  
+	        fileInput.focus();  
+	    }  
+	});
+	button.addEventListener( "click", function( event ) {
+	   fileInput.focus();
+	   return false;
+	});  
+	fileInput.addEventListener( "change", function( event ) {  
+	    the_return.innerHTML = this.value;  
+	});  
+ 	
 
 });
