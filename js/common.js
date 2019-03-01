a3548e6f0012f64743bb1b42bfb7ad1b13596e54
Breeze8 <stashenkodenis@gmail.com>
2019-03-01 13:26:28 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,91 +1,59 @@
 $(function() {
 
-	$('.slider-certeficat__slide').magnificPopup({
-	  type:'image',
-	  callbacks: {
-	    beforeOpen: function() {
-	       this.st.mainClass = this.st.el.attr('data-effect');
-	    }
-	  },
-	  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-	});
+    $('.accordion > li:eq(0) a').addClass('active').next().slideDown();
 
+    $('.accordion a').click(function(j) {
+        var dropDown = $(this).closest('li').find('.drop');
 
-    $('.main-slider').slick({
-    	rows: false,
-    	fade: true,
-    	 responsive: [
-	    {
-	      breakpoint: 767,
-	      settings: {
-	        arrows: false,
-	        dots: true
-	      }
-	    }
-	  ]
+        $(this).closest('.accordion').find('.drop').not(dropDown).slideUp();
+
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+        } else {
+            $(this).closest('.accordion').find('a.active').removeClass('active');
+            $(this).addClass('active');
+        }
+
+        dropDown.stop(false, true).slideToggle();
+
+        j.preventDefault();
+    });
+
+    $('.reviews-slider').slick({
+        rows: false,
+        arrows: false,
+        dots: true,
     })
 
-    $('.slider-top').slick({
-    	rows: false,
-    	responsive: [
-	    {
-	      breakpoint: 767,
-	      settings: {
-	        arrows: false,
-	        dots: true
-	      }
-	    }
-	  ]
-    })
+    $('.btn-modal').magnificPopup({
+      type:'inline',
+      removalDelay: 500, //delay removal by X to allow out-animation
+      fixedContentPos: true,
+      callbacks: {
+        beforeOpen: function() {
+           this.st.mainClass = this.st.el.attr('data-effect');
+        },
+        open: function() {
+            $('body').addClass('fixed')
+        },
+        close: function() {
+            $('body').removeClass('fixed')
+        },
+      },
+      midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    });
 
-    $('.slider-certeficat').slick({
-    	rows: false,
-    	slidesToShow: 4,
-    	 responsive: [
-		    {
-		      breakpoint: 992,
-		      settings: {
-		        arrows: false,
-		        dots: true,
-		        slidesToShow: 3,
-		      }
-		    },
-		    {
-		      breakpoint: 767,
-		      settings: {
-		        arrows: false,
-		        dots: true,
-		        slidesToShow: 2,
-		      }
-		    },
-		    {
-		      breakpoint: 480,
-		      settings: {
-		        arrows: false,
-		        dots: true,
-		        slidesToShow: 1,
-		      }
-		    },
-		  ]
-    })
+    $('.modal__close').click(function() {
+        $.magnificPopup.close();
+    });
 
     $('.hamburger').click(function() {
-      $(this).toggleClass('is-active');
-      $('.main-nav').slideToggle(function() {
-          if ($(this).css('display') === 'none') {
-              $(this).removeAttr('style');
-          }
+          $(this).toggleClass('is-active');
+          $('.main-nav').slideToggle(function() {
+              if ($(this).css('display') === 'none') {
+                  $(this).removeAttr('style');
+              }
+          });
       });
-  });
-
-    $(".tab_item").not(":first").hide();
-	$(".tab").click(function() {
-		$(".tab").removeClass("active").eq($(this).index()).addClass("active");
-		$(".tab_item").hide().eq($(this).index()).fadeIn()
-	}).eq(0).addClass("active");
-
-	if ($(window).width() < 767) {
-	   $('.header .btn').text('Записаться')
-	}
 
 });
